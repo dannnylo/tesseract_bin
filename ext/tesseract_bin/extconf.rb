58431b8217c157b2523fbb95b50458fd2fb16d23
Danilo Jeremias da Silva <dannnylo@gmail.com>
2012-01-29 15:48:06 -0200

    Vendor

--- ext/tesseract_bin/extconf.rb
+++ ext/tesseract_bin/extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 root = File.expand_path('../../..', __FILE__)
 
 Dir.chdir(File.join(root, 'vendor/tesseract-2.04')) do
-  system "./configure", "--prefix=#{root}"
+  system "./configure", "--prefix=#{root} --exec-prefix=#{root}"
   puts "make"
   system "make"
   puts "make install"
