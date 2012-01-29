ff007bdbc68f3fcd26b79129bdb435170ca5a676
Danilo Jeremias da Silva <dannnylo@gmail.com>
2012-01-29 15:17:32 -0200

    Initial commit to tesseract_bin.

---
+++ test/test_tesseract_bin.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestTesseractBin < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
