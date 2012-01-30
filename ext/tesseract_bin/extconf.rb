6ec4498f3959898b4fee2e67d0bcd9d9bd517d40
Danilo Jeremias da Silva <danilo.silva@euax.com.br>
2012-01-30 09:15:13 -0200

    Bin

--- ext/tesseract_bin/extconf.rb
+++ ext/tesseract_bin/extconf.rb
@@ -5,9 +5,8 @@ root = File.expand_path('../../..', __FILE__)
 Dir.chdir(File.join(root, 'vendor/tesseract-2.04')) do
   system "./configure", "--prefix=#{root}", "--exec-prefix=#{root}"
   system "make"
-  #system "make install"
-#  puts "make clean"
-#  system "make clean"
+  system "make install"
+  system "make clean"
 end
 
 create_makefile 'tesseract_bin'
--- lib/tesseract_bin.rb
+++ lib/tesseract_bin.rb
@@ -2,11 +2,10 @@ require 'pathname'
 
 module TesseractBin
   Root = Pathname.new(File.expand_path('../..', __FILE__))
-#  Bin  = Root.join('bin')
+  Bin  = Root.join('bin')
 
-#  Executables = Bin.children.inject({}) { |h, p|
-#    h[p.basename.to_s.to_sym] = p.to_s
-#    h
-#  }
+  Executables = Bin.children.inject({}) { |h, p|
+    h[p.basename.to_s.to_sym] = p.to_s
+    h
+  }
 end
-
