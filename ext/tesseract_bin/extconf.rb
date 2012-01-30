8c410d349e4d575666610db6e4d92089ce49064a
Danilo Jeremias da Silva <danilo.silva@euax.com.br>
2012-01-30 08:57:53 -0200

    Exec prefix

--- ext/tesseract_bin/extconf.rb
+++ ext/tesseract_bin/extconf.rb
@@ -3,15 +3,11 @@ require 'mkmf'
 root = File.expand_path('../../..', __FILE__)
 
 Dir.chdir(File.join(root, 'vendor/tesseract-2.04')) do
-  system "./configure", "--prefix=#{root} --exec-prefix=#{root}"
-  puts "make"
+  system "./configure", "--prefix=#{root}", "--exec-prefix=#{root}"
   system "make"
-  puts "make install"
   #system "make install"
-  puts root.inspect
 #  puts "make clean"
 #  system "make clean"
 end
 
 create_makefile 'tesseract_bin'
-
